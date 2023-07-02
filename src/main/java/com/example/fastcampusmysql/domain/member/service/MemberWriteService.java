076dd1420e6a13828ab19f468906fce3987125ef
jungwon <mac20010@naver.com>
2023-07-02 23:50:30 +0900

    feat: member 쓰기 Service 구현

--- src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
+++ src/main/java/com/example/fastcampusmysql/domain/member/service/MemberWriteService.java
@@ -1,10 +1,23 @@
 package com.example.fastcampusmysql.domain.member.service;
 
 import com.example.fastcampusmysql.domain.member.dto.RegisterMemberCommand;
+import com.example.fastcampusmysql.domain.member.entity.Member;
+import com.example.fastcampusmysql.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
+@Service
 public class MemberWriteService {
 
-    public void create(RegisterMemberCommand command){
+    private final MemberRepository memberRepository;
 
+    public Member create(RegisterMemberCommand command){
+        var member = Member.builder()
+                .nickname(command.nickname())
+                .email(command.email())
+                .birthday(command.birthday())
+                .build();
+        return memberRepository.save(member);
     }
 }
