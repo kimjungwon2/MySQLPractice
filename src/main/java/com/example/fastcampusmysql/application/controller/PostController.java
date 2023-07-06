69151985c3a8a71b9b178ac43771048c08585738
jungwon <mac20010@naver.com>
2023-07-07 00:00:36 +0900

    feat: Calendar 구현

---
+++ src/main/java/com/example/fastcampusmysql/application/controller/PostController.java
@@ -0,0 +1,34 @@
+package com.example.fastcampusmysql.application.controller;
+
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCount;
+import com.example.fastcampusmysql.domain.post.dto.DailyPostCountRequest;
+import com.example.fastcampusmysql.domain.post.dto.PostCommand;
+import com.example.fastcampusmysql.domain.post.entity.Post;
+import com.example.fastcampusmysql.domain.post.service.PostReadService;
+import com.example.fastcampusmysql.domain.post.service.PostWriteService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    private final PostWriteService postWriteService;
+    private final PostReadService postReadService;
+
+    @PostMapping("")
+    public Long create(PostCommand command){
+        return postWriteService.create(command);
+    }
+
+    @GetMapping("/daily-post-counts")
+    public List<DailyPostCount> getDailyPostCounts(@RequestBody DailyPostCountRequest request){
+        return postReadService.getDailyPostCounts(request);
+    }
+}
